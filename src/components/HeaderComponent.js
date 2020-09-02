1c8b0e88e4864a7409dcf94cc8eff2bf3ad3bc1c
Michaella Magtibay <michaeltmagtibay@gmail.com>
2020-09-02 10:44:35 +0700

    Import stylesheet and return div with className

--- src/components/HeaderComponent.js
+++ src/components/HeaderComponent.js
@@ -1,8 +1,10 @@
 import React from "react";
 
+import "../App.css";
+
 class Header extends React.Component {
   render() {
-    return <h1>Welcome to Ming's Library!</h1>;
+    return <h1 className="header">Welcome to Ming's Library!</h1>;
   }
 }
 
